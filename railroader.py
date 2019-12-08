a523030ca4d45dc0b2362cb6f59953bab4b97420
Vadim Zaytsev <vadim@grammarware.net>
2019-12-08 22:27:49 +0100

    exercises in SVG generation

---
+++ railroader.py
@@ -0,0 +1,165 @@
+#!/c/Users/vadim/AppData/Local/Programs/Python/Python37-32/python
+
+HOR_STEP = 20
+
+svg_head = '''
+<svg xmlns="http://www.w3.org/2000/svg"{0}>
+	<defs>
+		<style type="text/css">
+			@namespace "http://www.w3.org/2000/svg";
+			svg {{background-color: white;}}
+			path {{fill: none; stroke: black;}}
+			polygon {{fill: black; stroke: black;}}
+			text {{font-size:12px;fill:black;font-weight:bold;font-family:monospace;}}
+			text.i {{font-style:italic;}}
+		</style>
+	</defs>'''
+svg_end = '</svg>'
+
+def len_text_in_px(s):
+	return int(len(s)*7.5) + 2
+
+def len_seq_in_px(xs):
+	global HOR_STEP
+	cx = 0
+	for x in xs:
+		if x[0] == 'skip':
+			cx += HOR_STEP
+		elif x[0] == 'term' or x[0] == 'nt':
+			cx += len_text_in_px(x[1])
+	return cx
+
+def make_triangle(x, y, right=False, left=False, down=False, up=False):
+	# x is 33 vertical
+	# y is 9
+	if left or right:
+		mod = 8 if left else -8
+		return '<polygon points="{0} {2} {1} {3} {1} {4}"/>'.format(x, x+mod, y, y-4, y+4)
+	elif down or up:
+		mod = 8 if up else -8
+		return '<polygon points="{0} {3} {1} {4} {2} {4}"/>'.format(x, x-4, x+4, y, y+mod)
+
+def make_loop(x, y, height, dx):
+	r = '<path d="M {0} {1} v -{2} h -{3} v {2}"/>'.format(x, y, height, dx)
+	r += make_triangle(x-dx, y-1, down=True)
+	return r
+
+def make_step_down(x, y, height, dx):
+	r = '<path d="M {0} {1} v {2} h {3}"/>'.format(x, y, height, dx)
+	return r
+
+def make_step_back_up(x, y, height, dx):
+	r = '<path d="M {0} {1} h {3} v -{2}"/>'.format(x, y, height, dx)
+	r += make_triangle(x+dx, y-height+1, up=True)
+	return r
+
+def make_line(x, y, dx=None, dy=None):
+	d = ' '
+	if dx:
+		d += 'h {0} '.format(dx)
+	if dy:
+		d += 'v {0} '.format(dy)
+	return '<path d="M {0} {1} {2}"/>'.format(x, y, d.strip())
+
+def make_text(x, y, txt, nt=False):
+	s = ' class="i"' if nt else ''
+	return '<text{3} x="{0}" y="{1}">{2}</text>'.format(x, y+3, txt, s)
+
+def make_diag(dspec, y, x=HOR_STEP):
+	global HOR_STEP
+	res = []
+	res.append('\n<!-- {0} -->\n'.format(dspec))
+	for d in dspec:
+		if type(d) == type([]):
+			for r in make_diag(d, y):
+				res.append(r)
+		elif d[0] == 'begin':
+			x += 8
+			res.append(make_triangle(x, y, right=True))
+			x += 8
+			res.append(make_triangle(x, y, right=True))
+		elif d[0] == 'end':
+			x += 8
+			res.append(make_triangle(x, y, right=True))
+			res.append(make_triangle(x, y, left=True))
+			x += 8
+		elif d[0] == 'skip':
+			L = d[1] if len(d) > 1 else 1
+			res.append(make_line(x, y, dx=HOR_STEP*L))
+			x += HOR_STEP*L
+		elif d[0] == 'term':
+			x += 4;
+			res.append(make_text(x, y, d[1]))
+			x += len_text_in_px(d[1])
+		elif d[0] == 'nt':
+			res.append(make_text(x, y, d[1], nt=True))
+			x += len_text_in_px(d[1])
+		elif d[0] == 'uploop':
+			res.append(make_loop(x, y, HOR_STEP, dx=d[1]+2*HOR_STEP))
+		elif d[0] == 'optional':
+			length = len_seq_in_px(d[1]) + 2*HOR_STEP
+			res.append(make_line(x, y, dx=length))
+			res.append(make_step_down(x, y, HOR_STEP, HOR_STEP))
+			res.append(make_diag(d[1], y + HOR_STEP, x=x+HOR_STEP))
+			res.append(make_step_back_up(x+length-HOR_STEP, y+HOR_STEP, HOR_STEP, HOR_STEP))
+			x += length
+		elif d[0] == 'downloop':
+			print('[!] {0}'.format(d[1]))
+			length = len_seq_in_px(d[1]) + 4*HOR_STEP
+			res.append(make_line(x, y, dx=length))
+			res.append(make_step_down(x, y, 2*HOR_STEP, 2*HOR_STEP))
+			res.append(make_diag(d[1], y + 2*HOR_STEP, x=x+2*HOR_STEP))
+			res.append(make_step_back_up(x+length-2*HOR_STEP, y+2*HOR_STEP, 2*HOR_STEP, 2*HOR_STEP))
+			res.append(make_loop(x+length-HOR_STEP, y+2*HOR_STEP, HOR_STEP, dx=length-2*HOR_STEP))
+			x += length
+		else:
+			print('[?] ' + d[0])
+	return ''.join(res)
+# ('begin')
+# ('skip')
+# ('term', NAME)
+# ('uploop', 'actual text', 'length')
+# ('end')
+
+# TESTER
+if False:
+	with open('test.html', 'w', encoding='utf-8') as html:
+		html.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html version="-//W3C//DTD XHTML 1.1//EN" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.w3.org/1999/xhtml http://www.w3.org/MarkUp/SCHEMA/xhtml11.xsd"><head/><body>')
+		html.write('<svg xmlns="http://www.w3.org/2000/svg"><defs><style type="text/css">@namespace "http://www.w3.org/2000/svg"; path {fill: none; stroke: black;} polygon {fill: black; stroke: black;} svg {background-color: white;} text {font-size:12px;fill:black;font-weight:bold;font-family:monospace;} text.i {font-style:italic;}</style></defs>')
+		html.write(make_diag([\
+			('begin',),\
+			('skip',),\
+			('term', 'ACCEPT'),\
+			('skip', 2),\
+			('nt', 'Identifier'),\
+			('skip', ),\
+			('uploop', '', 'Identifier'),\
+			('skip', ),\
+			('end',),\
+			], 30))
+		html.write(make_diag([\
+			('begin',),\
+			('skip',),\
+			('term', 'A'),\
+			('skip',),\
+			('end',),\
+			], 50))
+		html.write(make_diag([\
+			('begin',),\
+			('skip',),\
+			('term', 'Aqwertyuiopasdfghjklzxcvbnm'),\
+			('skip',),\
+			('end',),\
+			], 70))
+		# html.write('<polygon points="9 33 1 29 1 37"/>')
+		# html.write(make_triangle(33    , 9, right=True))
+		# html.write(make_triangle(33 + 8, 9, right=True))
+		# html.write(make_text(55, 9, 'ACCEPT'))
+		# html.write(make_text(100, 9, 'Identifier', nt=True))
+		# html.write(make_triangle(200   , 9, right=True))
+		# html.write(make_triangle(200   , 9, left=True))
+		# html.write(make_triangle(110   , 9, up=True))
+		# html.write(make_triangle(120   , 9, down=True))
+		# html.write(make_line(33, 9, dx=20))
+		# html.write(make_triangle(30,20, right=True))
+		html.write('</body></html>')
--- roadmapper.py
+++ roadmapper.py
@@ -1,11 +1,13 @@
 #!/c/Users/vadim/AppData/Local/Programs/Python/Python37-32/python
 
 import sys, os, glob
+import railroader
 
 lang2stmt = {} # L -> S
 lang2srcs = {} # L -> text
 host2stmt = {} # (H)S -> L.S
 stmt2desc = {} # L.S -> D
+stmt2fmts = {} # L.S -> BNF
 stmt2host = {} # L.S -> (H)S
 set_of_used = set() # L.S
 
@@ -17,15 +19,19 @@ gen_tpl1 = '''<?xml version="1.0" encoding="UTF-8"?>
 		<header/>
 		<link href="{3}" rel="stylesheet" type="text/css" />
 		<img src="{4}" style="width:200px;height:200px;" class="flr" />
-		<h1><span class="ff lang"><a href="index.html">{0}</a></span>: The ===SUBTITLE===</h1>
+		<h1><span class="ff lang"><a href="index.html">{0}</a></span>: ===SUBTITLE===</h1>
 		<hr/>
 		<h2>Statements:</h2>
 		'''
-lang_tpl1 = gen_tpl1.replace('===TITLE===', '{0}: the {2} origins').replace('===SUBTITLE===', '<span class="ff lang"><a href="{2}.html">{1}</a></span> Origins')
-indx_tpl1 = gen_tpl1.replace('===TITLE===', '{0}').replace('===SUBTITLE===', 'Language Reference')
-lang_tpl2 = indx_tpl2 = '<span class="ff used"><a href="{2}.html" title="{1}">{0}</a></span> '
+lang_tpl1 = gen_tpl1.replace('===TITLE===', '{0}: the {2} origins').replace('===SUBTITLE===', 'The <span class="ff lang"><a href="{2}.html">{1}</a></span> Origins')
+indx_tpl1 = gen_tpl1.replace('===TITLE===', '{0}').replace('===SUBTITLE===', 'The Language Reference')
+stmt_tpl1 = gen_tpl1.replace('===TITLE===', '{0}: {1}').replace('===SUBTITLE===', '<span class="ff used"><a href="{2}.html">{1}</a></span>').\
+			split('<hr/>')[0] + '{5}'
+lang_tpl2 = indx_tpl2 = stmt_tpl5 = '<span class="ff used"><a href="{2}.html" title="{1}">{0}</a></span> '
+stmt_tpl2 = '<hr/><h2>Format:</h2>'
+stmt_tpl3 = '<h2>Origins:</h2>'
 lang_tpl3 = '<span class="ff" title="{1}">{0}</span> '
-indx_tpl3 = '<span class="ff lang"><a href="{1}.html">{0}</a></span>&nbsp;&nbsp;&nbsp;'
+indx_tpl3 = stmt_tpl4 = '<span class="ff lang"><a href="{1}.html">{0}</a></span>&nbsp;&nbsp;&nbsp;'
 lang_tpl4 = '''
 		<h2>Sources:</h2>
 		<ul>
@@ -42,7 +48,7 @@ lang_tpl5 = indx_tpl5 = '''
 </html>'''
 
 def dump_index(f, host, css, logo):
-	print('[LDR] dump_index({0}, ...)'.format(f.name))
+	print('[LDR] dump_index({0}, {1}, {2}, {3})'.format(f.name, host, css, logo))
 	f.write(indx_tpl1.format(host, 'ERROR', 'ERROR', css, logo))
 	for stmt in sorted(lang2stmt[host]):
 		f.write(indx_tpl2.format(stmt, get_desc(host, stmt), name2filename(stmt)))
@@ -51,7 +57,37 @@ def dump_index(f, host, css, logo):
 		if host == lang:
 			continue
 		f.write(indx_tpl3.format(lang, name2filename(lang)))
-	f.write(indx_tpl5)
+	f.write(indx_tpl5.format(host))
+
+def dump_statement(f, host, stmt, css, logo):
+	print('[LDR] dump_statement({0}, {1}, {2}, {3}, {4})'.format(f.name, host, stmt, css, logo))
+	f.write(stmt_tpl1.format(host, stmt, name2filename(stmt), css, logo, get_desc(host, stmt)))
+	f.write(stmt_tpl2)
+	fmt = get_fmt(host, stmt)
+	if fmt:
+		print(fmt)
+		if fmt[2][0] == 'size':
+			sizes = fmt[2][1].split('x')
+			f.write(railroader.svg_head.format(' width="{0}px" height="{1}px"'.format(sizes[0], sizes[1])))
+			fmt = fmt[0:1] + fmt[3:]
+		else:
+			f.write(railroader.svg_head.format(''))
+		f.write(railroader.make_diag(fmt, 30))
+		f.write(railroader.svg_end)
+	if stmt in host2stmt:
+		f.write(stmt_tpl3)
+		by_lang = {}
+		for s in host2stmt[stmt]:
+			a,b = s.split('.')
+			if a not in by_lang:
+				by_lang[a] = set()
+			by_lang[a].add(b)
+		for k in sorted(by_lang.keys()):
+			f.write(stmt_tpl4.format(k, name2filename(k)))
+			for s in sorted(by_lang[k]):
+				f.write(stmt_tpl5.format(s, get_desc(k, s), name2filename(k)))
+			f.write('<br/>')
+	f.write(indx_tpl5.format(host))
 
 def dump_language(f, host, lang, css, logo):
 	print('[LDR] dump_language({0}, {1}, {2}, {3}, {4})'.format(f.name, host, lang, css, logo))
@@ -80,6 +116,72 @@ def get_desc(lang, stmt):
 		return stmt2desc[key]
 	else:
 		return ''
+
+def get_fmt(lang, stmt):
+	key = combine(lang, stmt)
+	if key in stmt2fmts:
+		fmt = stmt2fmts[key]
+		bnf = []
+		bnf.append(('begin',))
+		for w in fmt.split():
+			f = fmt2rrd(w)
+			bnf.append(f)
+		bnf.append(('end',))
+		return widen(flatten(bnf))
+	else:
+		return ()
+
+cache = []
+caching = False
+
+def fmt2rrd(w):
+	global caching, cache
+	if w[0] == '@':
+		return [('size', w[1:])]
+	if caching:
+		if w == ')?':
+			caching = False
+			return [('optional', widen(flatten([fmt2rrd(x) for x in cache])))]
+		if w == ')*':
+			caching = False
+			return [('downloop', widen(flatten([fmt2rrd(x) for x in cache])))]
+		if w == ')+':
+			caching = False
+			r = flatten([fmt2rrd(x) for x in cache])
+			r.append(('uploop', railroader.len_seq_in_px(r)))
+			r.insert(0, ('skip',))
+			return r
+		cache.append(w)
+		return []
+	if w == '(':
+		caching = True
+		cache = []
+		return []
+	if w[-1] == '+':
+		return [('skip',), fmt2rrd(w[:-1]), ('uploop', railroader.len_text_in_px(w[:-1])), ('skip' ,)]
+	if w[0] == '"' and w[-1] == '"':
+		return [('term', w[1:-1])]
+	return [('nt', w)]
+
+def flatten(xs):
+	r = []
+	for x in xs:
+		if type(x) == type([]):
+			for y in flatten(x):
+				r.append(y)
+		else:
+			r.append(x)
+	return r
+
+def widen(xs):
+	r = []
+	last_skip = True
+	for x in xs:
+		if not last_skip:
+			r.append(('skip',))
+		r.append(x)
+		last_skip = x[0] == 'skip'
+	return r
 
 def add_used(lang, stmt, hoststmt):
 	key = combine(lang, stmt)
@@ -117,6 +219,8 @@ with open(sys.argv[1], 'r', encoding='utf-8') as ldr:
 			continue
 		if words[0] == 'summary:':
 			stmt2desc[combine(lang, stmt)] = line[8:].strip()
+		elif words[0] == 'syntax:':
+			stmt2fmts[combine(lang, stmt)] = line[8:].strip()
 		elif words[0] == 'statement':
 			if len(words) == 4:
 				if words[2] != 'became':
@@ -159,9 +263,8 @@ for lang in lang2stmt.keys():
 # all statements
 for stmt in lang2stmt[host]:
 	print('[LDR] host2stmt[{0}]'.format(stmt))
-	# with open(sys.argv[2] + name2filename(lang) + '.dsl', 'w', encoding='utf-8') as langfile:
-	# 	dump_language(langfile, host, lang, css, logo)
-
+	with open(sys.argv[2] + name2filename(stmt) + '.dsl', 'w', encoding='utf-8') as stmtfile:
+		dump_statement(stmtfile, host, stmt, css, logo)
 # host language file
 with open(sys.argv[2] + 'index.dsl', 'w', encoding='utf-8') as index:
 	dump_index(index, host, css, logo)
