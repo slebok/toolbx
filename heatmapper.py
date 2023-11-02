89fd1f089db93d75c7df8bfab60bdfc0ce902707
Vadim Zaytsev <vadim@grammarware.net>
2023-11-02 21:52:10 +0100

    Improve SLEBoK generation

--- heatmapper.py
+++ heatmapper.py
@@ -1,4 +1,5 @@
 #!/Users/grammarware/opt/anaconda3/envs/py11/bin/python3
+import glob
 
 # T02: Static Semantics;;;;;;;;;Static semantics (e.g., design rules, well-formedness constraints);Static semantics (e.g., design rules, well-formedness constraints);Static semantics (e.g., design rules, well-formedness constraints);Static semantics (e.g., design rules, well-formedness constraints);Static semantics (e.g. design rules, well-formedness constraints);Static semantics (e.g. design rules, well-formedness constraints);Static semantics (e.g. design rules, well-formedness constraints);Static semantics (e.g., design rules, well-formedness constraints)
 
@@ -10,7 +11,7 @@ with open('../cfpbok/SLEBoK.csv', 'r', encoding='utf-8') as csv:
 
 print(f'CSV with {len(table)} rows and {len(table[-1])} found.')
 
-htable = [[f'<strong>SLE {x}</strong>'] for x in table[1][:]]
+htable = [[f'<strong><a href="#sle{x}">SLE {x}</a></strong>'] for x in table[1][:]]
 htable[0][0] = ''
 ts = []
 
@@ -36,10 +37,12 @@ with open('test.html', 'w', encoding='utf-8') as html:
 		<meta name="viewport" content="initial-scale=1.0"/>
 		<title>SLEBoK</title>
 		<style>
-			td.yes {background-color: red;}
+			td.yes {background-color: darkgreen;}
 			dt {font-weight: bold;}
 			dt,dd {display: inline;}
 			dd::after {content: "\A"; white-space: pre;}
+			.box {text-decoration: underline;}
+			span.Tx a {color: green; font-weight: bold;}
 		</style>
 	</head>
 	<body>''')
@@ -65,4 +68,36 @@ with open('test.html', 'w', encoding='utf-8') as html:
 	# part 2
 	html.write('<h1>Topic List</h1>')
 	html.write('<dl>'+ '\n'.join(ts) + '</dl><br/>')
+	# part 3
+	html.write('<h1>Calls for Papers</h1>')
+	for dsl in sorted(glob.glob("../cfpbok/*.cfp")):
+		year = dsl.split('/')[-1].split('.')[0]
+		html.write(f'<h2><a name="sle{year}"></a>SLE {year} Topics</h2>')
+		in_list = False
+		with open(dsl, 'r', encoding='utf-8') as dslf:
+			for line in dslf.readlines():
+				line = line.strip()
+				if not line:
+					continue
+				if line.startswith('http'):
+					html.write('<h6><a href="{line}">Source</a></h6>\n')
+				elif line.startswith('- '):
+					if not in_list:
+						in_list = True
+						html.write('<ul>')
+					text = line[2:]
+					while text.find('<T') > -1:
+						parts = text.split('<T')
+						parts[1] = '>'.join(parts[1].split('>')[1:])
+						text = parts[0] + '<span class="box">' + '<T'.join(parts[1:])
+					while text.find('</T') > -1:
+						parts = text.split('</T')
+						n = parts[1].split('>')[0]
+						parts[1] = '>'.join(parts[1].split('>')[1:])
+						text = parts[0] + f'</span><sup><span class="Tx"><a href="#T{n}">T{n}</a></span></sup>' + '</T'.join(parts[1:])
+					html.write(f'<li>{text}</li>\n')
+			if in_list:
+				html.write('</ul>\n')
+
+
 	html.write('</body></html>')
